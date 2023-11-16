1aa2f4b9e9ce20cc00eeeaac6b142b8ff6565e69
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-11-16 16:18:02 -0300

    Alteração 16.11

--- admin/Infos/quemsomos.php
+++ admin/Infos/quemsomos.php
@@ -3,23 +3,14 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
 
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Quem Somos - BuscaTec</title>
-</head>
+<main>
 <body>
     <header>
-
     </header>
 
 </body>
 </html>
+</main>
 
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/rodape.php";
