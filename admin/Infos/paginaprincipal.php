cd000fcdfe60779927cdd0af203b85f49f4bf7c5
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-11-17 17:08:12 -0300

    Alterações 17.08

---
+++ admin/Infos/paginaprincipal.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
+
+?>
+<main>
+
+    <body>
+        <div class="office">
+            <div class="trabalho">
+                <img src="/assets/img/program.png" height="200" width="300" alt="">
+            </div>
+            <div class="trabalho">
+                <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+            </div>
+            <div class="trabalho">
+                <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+            </div>
+        </div>
+        <div class="container">
+            <div class="owl-carousel owl-theme">
+                <div class="/assets/img/program.png">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+            </div>
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/designer.jpg" height="200" width="300" alt="">
+                </div>
+            </div>
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+                <div class="item">
+                    <img src="/assets/img/técnico.jpg" height="200" width="300" alt="">
+                </div>
+            </div>
+        </div>
+
+
+    </body>
+
+    </html>
+</main>
+
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/rodape.php";
+?>
--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="/assets/css/main.css">
+   
 
     <title>BuscaTec</title>
 </head>
+
 <body>
     <header>
         <div class="containerprincipal">
             <div class="logo-imagem"><img src="/assets/img/png" alt=""></div>
-            <div class="textoh1"> <h1>BuscaTec</h1></div>
+            <div class="textoh1">
+                <h1>BuscaTec</h1>
+            </div>
         </div>
 
         <div class="menu">
             <ul>
-                <li><a href="/">Inicio</a></li>
+                <li><a href="/admin/Infos/inicio.php">Inicio</a></li>
                 <li><a href="/admin/Infos/guia.php">Guia</a></li>
                 <li><a href="/admin/Infos/quemsomos.php">Quem somos</a></li>
                 <li><a href="/">Planos</a></li>
